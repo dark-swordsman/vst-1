a3daa7dc5bd17d8001002113ab00dc02fda9d476
dark-swordsman <kylebarr54321@gmail.com>
2021-06-30 20:18:47 -0500

    test subprocess with array command

--- ts/test2.py
+++ ts/test2.py
@@ -1,11 +1,10 @@
-import os
+import subprocess
 
-input = "test-video-1.mp4"
+input = "-i test-video-1.mp4"
 output = "test-output.mp4"
 filters = "-vf scale=-1:720:flags=lanczos"
 options = "-c:v libx264 -crf 23 -preset veryfast -c:a aac -b:a 160k"
 
+command = ["ffmpeg"] + input.split() + filters.split() + options.split() + [output]
 
-os.system(f"ffmpeg -i {input}{' ' + filters if filters else ''}{' ' + options if options else ''} {output}")
-
-# maybe convert to array with join?
+subprocess.run(command)
