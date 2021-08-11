0eacc71c7c6b68906a907564c0d8f8137fb42456
dark-swordsman <kylebarr54321@gmail.com>
2021-08-11 15:25:18 -0600

    created basic file upload with writeable stream

--- api/server.js
+++ api/server.js
@@ -1,8 +1,27 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const fs = require('fs')
+
+const app = express();
 
 const PORT = 3010;
 
 app.get('/', (req, res) => res.send('yeet'));
+
+app.post('/upload', (req, res) => {
+  try {
+    const filePath = __dirname + '/temp/write.mp4';
+    const stream = fs.createWriteStream(filePath);
+  
+    req.pipe(stream);
+
+    req.on('end', () => {
+      stream.end();
+      res.end();
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+});
 
 app.listen(PORT, () => console.log(`API running on ${PORT}`));
